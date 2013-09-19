fc07e98e27e6304c486e885fb10ce89a8ef71e87
david karapetyan <david@lappy.(none)>
2013-09-19 16:11:21 -0700

    added proc to evaled structures

--- let/values.rb
+++ let/values.rb
@@ -3,5 +3,6 @@ module LetGrammar
   class NumVal < Struct.new(:value); end
   class BoolVal < Struct.new(:value); end
   class ListVal < Struct.new(:value); end
+  class ProcVal < Struct.new(:value, :env); end
 
 end
